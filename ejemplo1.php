04a0c88a053b578506c3f0621bcdc490a79dec26
Administrator <rmenchaca@toditocard.com>
2015-04-10 12:28:49 -0500

    php vulnerable 1.3

--- ejemplo1.php
+++ ejemplo1.php
@@ -1,7 +1,7 @@
 <?php
 
-$xx=$_GET['x'];
-file_get_contents($x);
+$archivo = filter_input(INPUT_GET, 'xx', FILTER_SANITIZE_ENCODED);
+file_get_contents($archivo);
 exec($x);
 while(1){
 	echo ("hello");
